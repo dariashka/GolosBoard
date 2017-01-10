544c6f27dcf784b32de926c9e2477394bd57baec
qqcky <darya.baklanova@gmail.com>
2017-01-11 01:08:18 +0700

    added html-code for popup with description of the first badge

--- scripts/app.js
+++ scripts/app.js
@@ -22,11 +22,31 @@
     var Description = [{
         id: "firstpost",
         title: "Первый пост",
-        description: "Напиши свой первый пост на Голосе и получи бейдж"
+        description: "<h1>Первый пост</h1><p>Напиши свой первый пост и получи бейдж.</p><iframe width='770' height='433' src='https://www.youtube.com/embed/uGF2kUcOefg' frameborder='0' allowfullscreen></iframe>"
     }, {
         id: "firstcomment",
         title: "Первый комментарий",
         description: "Напиши свой первый комментарий и получи бейдж"
+    }, {
+        id: "author",
+        title: "xxx",
+        description: "yyyyy"
+    }, {
+        id: "comment",
+        title: "xxx",
+        description: "yyyyy"
+    }, {
+        id: "curator",
+        title: "xxx",
+        description: "yyyyy"
+    }, {
+        id: "hit_top_comment",
+        title: "xxx",
+        description: "yyyyy"
+    }, {
+        id: "hit_top_payout",
+        title: "xxx",
+        description: "yyyyy"
     }];
 
 })();
