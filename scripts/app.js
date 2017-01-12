a8c5a0f3619f90ca48cde790ae961a61cf6577db
VIM-Arcange <VIM-Arcange@users.noreply.github.com>
2017-01-13 00:21:11 +0100

    All badges added
    I have added all badges filenames
    File (.png) have also been uploaded and can be displayed for each users

--- scripts/app.js
+++ scripts/app.js
@@ -19,34 +19,30 @@
     //var username = angular.element($('#golos-username')).val();
 
 
-    var Description = [{
-        id: "firstpost",
-        title: "Первый пост",
-        description: "<h1>Первый пост</h1><p>Напиши свой первый пост и получи бейдж.</p><iframe width='770' height='433' src='https://www.youtube.com/embed/uGF2kUcOefg' frameborder='0' allowfullscreen></iframe>"
-    }, {
-        id: "firstcomment",
-        title: "Первый комментарий",
-        description: "Напиши свой первый комментарий и получи бейдж"
-    }, {
-        id: "author",
-        title: "xxx",
-        description: "yyyyy"
-    }, {
-        id: "comment",
-        title: "xxx",
-        description: "yyyyy"
-    }, {
-        id: "curator",
-        title: "xxx",
-        description: "yyyyy"
-    }, {
-        id: "hit_top_comment",
-        title: "xxx",
-        description: "yyyyy"
-    }, {
-        id: "hit_top_payout",
-        title: "xxx",
-        description: "yyyyy"
-    }];
+    var Description = [
+    { id: "firstpost", title: "firstpost", description: "yyyyy" },
+    { id: "firstcomment", title: "firstcomment", description: "yyyyy" },
+    { id: "firstpayout", title: "firstpayout", description: "yyyyy" },
+    { id: "posts", title: "posts", description: "yyyyy" },
+    { id: "comments", title: "comments", description: "yyyyy" },
+    { id: "comments_in", title: "comments_in", description: "yyyyy" },
+    { id: "votes", title: "votes", description: "yyyyy" },
+    { id: "votes_in", title: "votes_in", description: "yyyyy" },
+    { id: "payout", title: "payout", description: "yyyyy" },
+    { id: "post4day", title: "post4day", description: "yyyyy" },
+    { id: "postallweek", title: "postallweek", description: "yyyyy" },
+    { id: "postallmonth", title: "postallmonth", description: "yyyyy" },
+    { id: "topvoteday", title: "topvoteday", description: "yyyyy" },
+    { id: "topvoteweek", title: "topvoteweek", description: "yyyyy" },
+    { id: "topcommentday", title: "topcommentday", description: "yyyyy" },
+    { id: "topcommentweek", title: "topcommentweek", description: "yyyyy" },
+    { id: "toppayoutday", title: "toppayoutday", description: "yyyyy" },
+    { id: "toppayoutweek", title: "toppayoutweek", description: "yyyyy" },
+    { id: "next_post", title: "next_post", description: "yyyyy" },
+    { id: "next_comment", title: "next_comment", description: "yyyyy" },
+    { id: "next_vote", title: "next_vote", description: "yyyyy" },
+    { id: "next_payout", title: "next_payout", description: "yyyyy" }
+	];
+
 
 })();
