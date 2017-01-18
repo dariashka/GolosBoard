e6fec14cb0b324927c4d12ed8ebc6e3de5c437d1
Darya Baklanova <darya.baklanova@gmail.com>
2017-01-18 14:40:16 +0700

    Update to gh-pages

--- scripts/app.js
+++ scripts/app.js
@@ -3,22 +3,27 @@
     var app = angular.module('golosBoard', ['ngSanitize']);
 
 
-    app.controller('GolosBoardController', function ($scope, $http, $location) {
+    app.controller('GolosBoardController', function ($scope, $log, $http, $location) {
         this.user = $location.search().user;
         this.descs = Description;
         this.stats = Statistics;
         this.nexts = NextBadges;
 
+        $scope.url = "https://golos-chain.arcange.eu/userstats.php?name=" + this.user;
 
-        $scope.url = "http://golos-chain.arcange.eu/userstats.php?name=" + this.user;
-        this.statData = [];
+        $http.get($scope.url)
+            .then(function (response) {
+                $scope.statsData = response.data;
+            });
 
-
-
-        $http.get($scope.url).then(function (response) {
-            this.statData = response.data;
-        });
-
+        this.updateStats = function () {
+            this.user = angular.element($('#golos-username')).val().toLowerCase();
+            $scope.url = "https://golos-chain.arcange.eu/userstats.php?name=" + this.user;
+            $http.get($scope.url)
+                .then(function (response) {
+                    $scope.statsData = response.data;
+                });
+        };
 
     });
 
@@ -75,103 +80,103 @@
         {
             id: "firstpayout",
             title: "Первая выплата",
-            description: "yyyyy"
+            description: "<p>Получите первую выплату на Голосе и бейдж в придачу.</p>"
         },
         {
             id: "posts",
             title: "Посты",
-            description: "yyyyy"
+            description: "<p>Пишите посты и получайте новые бейджи за бОльшее их количество.</p>"
         },
         {
             id: "comments",
-            title: "Комментарии",
-            description: "yyyyy"
+            title: "Вы комментируете",
+            description: "<p>Пишите комментарии и получайте новые бейджи за бОльшее их количество.</p>"
         },
         {
             id: "comments_in",
-            title: "comments_in",
-            description: "yyyyy"
+            title: "Вас комментируют",
+            description: "<p>Общайтесь с другими пользователями и получайте новые бейджи за количество входящих комментариев.</p>"
         },
         {
             id: "votes",
-            title: "votes",
-            description: "yyyyy"
+            title: "Вы голосуете",
+            description: "<p>Голосуйте за посты на Голосе и получайте новые бейджи за бОльшее их количество.</p>"
         },
         {
             id: "votes_in",
-            title: "votes_in",
-            description: "yyyyy"
+            title: "За вас голосуют",
+            description: "<p>Пишите больше классных постов и собирайте много голосов, а с ними и новые бейджи за все бОльшее их количество.</p>"
         },
         {
             id: "payout",
-            title: "payout",
+            title: "Выплата",
             description: "yyyyy"
         },
         {
             id: "post4day",
-            title: "post4day",
-            description: "yyyyy"
+            title: "4 поста за день",
+            description: "<p>Напишите четыре поста за один день и получите награду.</p>"
         },
         {
             id: "postallweek",
-            title: "postallweek",
-            description: "yyyyy"
+            title: "Посты каждый день всю неделю",
+            description: "<p>Держите ритм! Пишите посты каждый день в течение недели, и награда станет вашей.</p>"
         },
         {
             id: "postallmonth",
-            title: "postallmonth",
-            description: "yyyyy"
+            title: "Посты каждый день весь месяц",
+            description: "<p>Держите ритм! Пишите посты каждый день в течение месяца, и награда станет вашей.</p>y"
         },
         {
             id: "topvoteday",
-            title: "topvoteday",
-            description: "yyyyy"
+            title: "Лучший пост по количеству голосов за день",
+            description: "<p>Ваш пост набрал больше всего голосов на платформе за день? Награда ваша!</p>"
         },
         {
             id: "topvoteweek",
-            title: "topvoteweek",
-            description: "yyyyy"
+            title: "Лучший пост по количеству голосов за неделю",
+            description: "<p>Ваш пост набрал больше всего голосов на платформе за неделю? Награда ваша!</p>"
         },
         {
             id: "topcommentday",
-            title: "topcommentday",
-            description: "yyyyy"
+            title: "Самый обсуждаемый пост за день",
+            description: "<p>Ваш пост обсуждали на платформе больше всех за день? Награда ваша!</p>"
         },
         {
             id: "topcommentweek",
-            title: "topcommentweek",
-            description: "yyyyy"
+            title: "Самый обсуждаемый пост за неделю",
+            description: "<p>Ваш пост обсуждали на платформе больше всех за неделю? Награда ваша!</p>"
         },
         {
             id: "toppayoutday",
-            title: "toppayoutday",
-            description: "yyyyy"
+             title: "Самый дорогой пост за день",
+            description: "<p>Ваш пост стал самым высокооплачиваемым за день? Награда ваша!</p>"
         },
         {
             id: "toppayoutweek",
-            title: "toppayoutweek",
-            description: "yyyyy"
-        }
+            title: "Самый дорогой пост за неделю",
+            description: "<p>Ваш пост стал самым высокооплачиваемым за неделю? Награда ваша!</p>"
+        },
 	];
     var NextBadges = [{
             id: "next_post",
-            title: "next_post",
-            description: "yyyyy"
+            title: "Еще больше постов",
+            description: "<p>Пишите посты и получайте новые бейджи за бОльшее их количество.</p>"
         },
         {
             id: "next_comment",
-            title: "next_comment",
-            description: "yyyyy"
+            title: "Еще больше комментариев",
+            description: "<p>Пишите комментарии и получайте новые бейджи за бОльшее их количество.</p>"
         },
         {
             id: "next_vote",
-            title: "next_vote",
-            description: "yyyyy"
+            title: "Еще больше голосов",
+            description: "<p>Голосуйте больше и получайте новые бейджи за бОльшее их количество.</p>"
         },
         {
             id: "next_payout",
-            title: "next_payout",
-            description: "yyyyy"
+            title: "Больше денег!",
+            description: "<p>Пишите посты, комментарии, ставьте голоса и общайтесь с другими пользователями. Зарабатывайте деньги и новые бейджи в придачу.</p>"
         }]
 
 
